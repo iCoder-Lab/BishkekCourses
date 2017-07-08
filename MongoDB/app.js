af9fb13c77349a357aeb88531e494808a47ac897
Begimai Amantaeva <begimai.amantaeva@gmail.com>
2017-07-08 15:55:05 +0600

    Update app.js

--- MongoDB/app.js
+++ MongoDB/app.js
@@ -1,9 +1,11 @@
 var express = require('express')
 var app = express()
-var index = require('./controllers/index')
+var posts = require('./controllers/postRequests')
+var gets = require('./controllers/getRequests')
 app.set('view engine', 'ejs')
 app.use(express.static('./assets'))
 
-index(app)
+posts(app)
+gets(app)
 app.listen(3000, '')
 console.log('Listening -> 3000');
