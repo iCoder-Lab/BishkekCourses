6e173a87e80090a719561ef2f819285fec563504
Begimai Amantaeva <begimai.amantaeva@gmail.com>
2017-07-08 15:52:02 +0600

    Add files via upload

---
+++ MongoDB/controllers/getRequests.js
@@ -0,0 +1,71 @@
+var app = require('express')()
+var bP = require('body-parser').json()
+var mongoose = require('mongoose')
+mongoose.Promise = require('bluebird');
+var url = 'mongodb://127.0.0.1/courses'
+mongoose.connect(url, {useMongoClient: true});
+var models = require('../database/models/course')(mongoose)
+
+module.exports = function(app)
+{
+  app.get('/getAllCourses', function(request, response)
+  {
+    models.Course
+      .find({}).select({"name": 1, "_id": 2, "images.imagePath": 3})
+      .then(function(branches) {
+        response.send(branches)
+      })
+      .catch(function(error) {
+        response.send("error")
+      })
+  })
+
+  app.get('/getAllBranches', function(request, response)
+  {
+    models.Course
+      .find({}).select({"branches": 1, "address": 2, "longtitude": 3, "latitude": 4, "phone": 5, "_id": 6, "name": 7})
+      .then(function(branches) {
+        response.send(branches)
+      })
+      .catch(function(error) {
+        response.send("error")
+      })
+  })
+
+  app.get('/getAllCategories', function(request, response)
+  {
+    models.Course
+      .find({}).select({"category._id": 1, "subCategory._id": 2, "category.name": 3, "category.subCategory.name": 4, "images.imagePath": 5})
+      .then(function(branches) {
+        response.send(branches)
+      })
+      .catch(function(error){
+        response.send("error")
+      })
+  })
+  app.get('/getAll', function(request, response)
+  {
+    models.Course
+      .find({})
+      .then(function(result) {
+        response.send(result)
+      })
+      .catch(function(error){
+        console.log(error)
+        response.send("error")
+      })
+  })
+  app.get('/getCourse/:categoryID/:subcatID', function(request, response)
+  {
+    models.Course
+      .find({ $and: [{'category._id': request.params.categoryID}, {'category.subCategory._id': request.params.subcatID}]})
+      .select({'_id': 1, 'name': 2, 'images': 3, 'branches.address': 4, 'services.name': 5})
+      .then(function(result) {
+        response.send(result)
+      })
+      .catch(function(error){
+        console.log(error)
+        response.send("error")
+      })
+  })
+}
---
+++ MongoDB/controllers/postRequests.js
@@ -0,0 +1,43 @@
+var app = require('express')()
+var bP = require('body-parser').json()
+var mongoose = require('mongoose')
+mongoose.Promise = require('bluebird');
+var url = 'mongodb://127.0.0.1/courses'
+mongoose.connect(url, {useMongoClient: true});
+var models = require('../database/models/models')(mongoose)
+
+module.exports = function(app)
+{
+  app.post('/addCourse', bP, function(request, response)
+  {
+    var inp = request.body
+    var course = new models.Course(inp)
+
+    models.Course
+      .find({'name': inp.name}).limit(1)
+      .then(function(result)
+      {
+        if(result.length != 0)
+        {
+          response.send({error:'Muhamed, No No. That course already exists...'})
+        }
+        else
+        {
+          course.save(function (err, c)
+          {
+            if (err)
+              return console.send({error:err})
+            response.json(course)
+          })
+        }
+      })
+      .catch(function(error)
+      {
+        response.send({error:'There was an Internal Error'})
+      })
+  })
+  app.post('/addCategory', bP, function(request, response)
+  {
+
+  }
+}
